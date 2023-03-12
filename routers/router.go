26a6737e748a23de69154c3af9a089589a0b9177
Lithium_peko <lithiumreona@gmail.com>
2023-03-12 16:02:52 +0800

    Initial commit

---
+++ routers/router.go
@@ -0,0 +1,54 @@
+package routers
+
+import (
+	"gin/middleware/jwt"
+	"gin/pkg/upload"
+	"gin/routers/api"
+	v1 "gin/routers/api/v1"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
+)
+
+// InitRouter initialize routing information
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	r.StaticFS("/upload/images", http.Dir(upload.GetImageFullPath()))
+
+	r.GET("/auth", api.GetAuth)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.POST("/upload", api.UploadImage)
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		// 获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		// 新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		// 更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		// 删除指定标签
+		apiv1.DELETE("/tags/:id")
+		// 获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		// 获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		// 新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		// 更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		// 删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+	}
+
+	return r
+}
